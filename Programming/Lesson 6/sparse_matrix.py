a59ec95afee4327170ad5322c12e276bfec7cc48
dashaserdyuk <dashaisfunny@yandex.ru>
2016-10-18 00:33:39 +0300

    add multiply sparse matrix

--- Programming/Lesson 6/sparse_matrix.py
+++ Programming/Lesson 6/sparse_matrix.py
@@ -49,12 +49,45 @@ def access(row, column, mdict):
     else:
         return 0
 
-#TODO:
-# new representation for  multiplication matrix
-# read/write/access
-# multiplication
+
+def get_representation_by_row_from_dict(matrix):
+    repr_dict = {}
+    for key in matrix.keys():
+        if key[0] not in repr_dict:
+            repr_dict[key[0]] = []
+        repr_dict[key[0]].append((key[1], matrix[key]))
+    return repr_dict
 
 
-#read_big_matrix()
-matrix = read_my_big_matrix()
-print(access(9920, 7976, matrix))
+
+def get_representation_by_column_from_dict(matrix):
+    repr_dict = {}
+    for key in matrix.keys():
+        if key[1] not in repr_dict:
+            repr_dict[key[1]] = []
+        repr_dict[key[1]].append((key[0], matrix[key]))
+    return repr_dict
+
+
+def multiply(_m1, _m2):
+    m1 = get_representation_by_row_from_dict(_m1)
+    m2 = get_representation_by_column_from_dict(_m2)
+    print(m1)
+    print()
+    print(m2)
+    print()
+    result = {}
+    for i in range(len(m1)):
+        for j in range(len(m2)):
+            res = 0
+            for sub_i in m1[i]:
+                for sub_j in m2[j]:
+                    if (sub_i[0] == sub_j[0]):
+                        res += sub_i[1] * sub_j[1]
+            if res > 0:
+                result[(i,j)] = res
+    return result
+
+
+
+#TODO: make a big and beautiful class!!!! 
